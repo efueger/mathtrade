923e7240a567b58b569fa97d192c76e7156501e1
Edgar Sanchez <edgar.sanchez@atrapalo.com>
2015-03-21 01:30:14 +0100

    Change folder structure to a DDD like one

--- src/Mathtrade/Infrastructure/Ui/Web/Silex/Public/index.php
+++ src/Mathtrade/Infrastructure/Ui/Web/Silex/Public/index.php
@@ -1,20 +1,17 @@
-<?php 
+<?php
 // web/index.php
-require_once __DIR__.'/../vendor/autoload.php';
-
+require_once __DIR__.'/../../../../../../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views',
+    'twig.path' => __DIR__.'/../../Twig/Views',
 ));
-
 
 // ... definitions
 $app->get('/', function (Silex\Application $app) {
-   return $app['twig']->render('index.twig', array(
+    return $app['twig']->render('index.twig', array(
         'name' => 'edgard',
     ));
 });
 
 $app->run();
-?>
