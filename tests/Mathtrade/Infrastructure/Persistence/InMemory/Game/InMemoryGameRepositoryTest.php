ba23412573b799b4da3a55942a6ae3cb752318fe
Edgar Sanchez <edgar.sanchez@atrapalo.com>
2016-01-13 22:15:18 +0100

    add test coverage

--- tests/Mathtrade/Infrastructure/Persistence/InMemory/Game/InMemoryGameRepositoryTest.php
+++ tests/Mathtrade/Infrastructure/Persistence/InMemory/Game/InMemoryGameRepositoryTest.php
@@ -13,6 +13,10 @@ class InMemoryGameRepositoryTest extends \PHPUnit_Framework_TestCase
     const A_GAME_DESCRIPTION = 'description';
     const A_USER_NAME = 'user';
     const USER_NAME_WITH_GAMES = 'userGames';
+    const A_BGG_ID = 12;
+    const A_DESCRIPTION = 'A description';
+    const COLLECTION_ID = 54;
+    const THUMBNAIL = 'thumbnail';
 
     /** @var InMemoryGameRepository */
     protected $repo;
@@ -20,7 +24,11 @@ class InMemoryGameRepositoryTest extends \PHPUnit_Framework_TestCase
     public function  setUp() {
 
         /** @var  Game */
-        $game=new Game(self::A_GAME_ID, self::A_GAME_NAME, self::A_GAME_DESCRIPTION);
+        $game=new Game(self::A_GAME_ID, self::A_GAME_NAME);
+        $game->setBoardGameGeekId(self::A_BGG_ID);
+        $game->setDescription(self::A_DESCRIPTION);
+        $game->setCollectionId(self::COLLECTION_ID);
+        $game->setThumbnail(self::THUMBNAIL);
         $repo = array();
         $repo[self::USER_NAME_WITH_GAMES] = array($game);
 
@@ -55,6 +63,11 @@ class InMemoryGameRepositoryTest extends \PHPUnit_Framework_TestCase
         $games = $this->repo->findByUsername(self::USER_NAME_WITH_GAMES);
         $this->assertNotEmpty($games);
         $this->assertEquals($games[0]->name(), self::A_GAME_NAME);
+        $this->assertEquals($games[0]->id(), self::A_GAME_ID);
+        $this->assertEquals($games[0]->boardGameGeekId(), self::A_BGG_ID);
+        $this->assertEquals($games[0]->thumbnail(), self::THUMBNAIL);
+        $this->assertEquals($games[0]->collectionId(), self::COLLECTION_ID);
+        $this->assertEquals($games[0]->description(), self::A_DESCRIPTION);
     }
 
 }
