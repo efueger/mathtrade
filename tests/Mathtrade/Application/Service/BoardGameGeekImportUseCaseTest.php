bf1e20342bf3284b8b8c03360ee76d4933964447
Edgar Sanchez <edgar.sanchez@atrapalo.com>
2016-01-13 15:17:54 +0100

    fix case sensitive issue

--- tests/Mathtrade/Application/Service/BoardGameGeekImportUseCaseTest.php
+++ tests/Mathtrade/Application/Service/BoardGameGeekImportUseCaseTest.php
@@ -2,8 +2,7 @@
 
 namespace EdySanchez\Mathtrade\Test\Application\Service;
 
-
-use Edysanchez\Mathtrade\Application\Service\BoardGamegeekImport\BoardGameGeekImportRequest;
+use Edysanchez\Mathtrade\Application\Service\BoardGameGeekImport\BoardGameGeekImportRequest;
 use Edysanchez\Mathtrade\Application\Service\BoardGameGeekImport\BoardGameGeekImportUseCase;
 use Edysanchez\Mathtrade\Domain\Model\Game\Game;
 use Edysanchez\Mathtrade\Domain\Model\Game\GameRepository;
